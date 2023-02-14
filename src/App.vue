7d98b09afca51fc8e53efdcc8ddbb3ea226f1f6b
laoer536 <1547698569@qq.com>
2023-02-15 00:13:21 +0800

    feat: colors add

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@
 <script setup lang="ts"></script>
 
 <style lang="scss">
+@use 'sass:map';
 p {
   @include more-line-ellipsis(1);
+  background-color: get-color($rose, 500, 0.3);
+  color: map.get($orange, 600);
 }
 </style>
